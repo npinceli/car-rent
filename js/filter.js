564bfca31ecad0a93d53243e6d08bd0fcf192823
Nicolas Pinceli <nicolaspinceli@gmail.com>
2023-06-08 16:27:46 -0300

    all cars

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,9 @@
 const brandButton = document.querySelectorAll('.btn')
 brandButton.forEach (btn => btn.addEventListener('click', carFilter))
 
+const all = document.querySelector('.all-cars')
+all.addEventListener('click', allCars)
+
 function carFilter () {
     const idBtn = document.getElementById(this.id)
     const brandBtn = idBtn.value
@@ -8,4 +11,10 @@ function carFilter () {
     let carFiltered = cars.filter(car => car.brand == brandBtn)
 
     showCars(carFiltered)
+}
+
+function allCars() {
+    let allCars = cars
+
+    showCars(allCars)
 }
--- js/main.js
+++ js/main.js
@@ -27,3 +27,4 @@ function showCars (carList) {
         `
     })
 }
+

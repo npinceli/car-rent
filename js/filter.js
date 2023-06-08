9784932f1f77d45a2bea661fc0946eb89ee112f5
Nicolas Pinceli <nicolaspinceli@gmail.com>
2023-06-08 16:00:05 -0300

    filter method

---
+++ js/filter.js
@@ -0,0 +1,11 @@
+const brandButton = document.querySelectorAll('.btn')
+brandButton.forEach (btn => btn.addEventListener('click', carFilter))
+
+function carFilter () {
+    const idBtn = document.getElementById(this.id)
+    const brandBtn = idBtn.value
+
+    let carFiltered = cars.filter(car => car.brand == brandBtn)
+
+    showCars(carFiltered)
+}
--- js/main.js
+++ js/main.js
@@ -5,6 +5,25 @@ let cars = []
     async function getSearchCars() { 
         const res = await fetch(dataBase)
         cars = await res.json()
-    
-        console.log(cars)
+        showCars(cars)
+}
+
+const insertCars = document.getElementById('cars') 
+
+function showCars (carList) {
+    insertCars.innerHTML = ''
+    carList.forEach( car => {
+        insertCars.innerHTML += `
+            <div class="cars-content-cards-car">
+                <img class="cars-content-cards-image" src="${car.image}" alt="">
+                <span class="cars-content-cards-brand tag flex"> <span class="material-symbols-outlined">settings</span> <p>${car.brand}</p> </span>
+                <span class="cars-content-cards-type tag flex"><span class="material-symbols-outlined">key</span> <p>${car.type}</p></span>
+                <span class="cars-content-cards-year tag flex"> <span class="material-symbols-outlined">calendar_month</span>  <p>${car.year}</p> </span>          
+                <h2 class="cars-content-cards-name">${car.name}</h2>
+                <p class="cars-content-cards-description">${car.desc}</p>
+                <p class="cars-content-cards-price">$ ${car.price}/day</p>
+                <a href="" class="cars-content-cards-btn">Rent Now</a>
+            </div>
+        `
+    })
 }
